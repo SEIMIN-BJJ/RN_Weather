f4ce439a62325ebbf7ec9217ded1a52cec8b8d5c
SEIMIN-BJJ <interkq12@gmail.com>
2024-03-18 17:59:35 +0900

    .

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
-import React from 'react';
 import * as Location from 'expo-location';
-import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Dimensions, ActivityIndicator, StyleSheet, ScrollView } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
 
-const screenWidth = Dimensions.get('window').width;
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
+
 const API_KEY = 'e8e04ff2b6daa7caef341391331e9e7c';
+
+const icons = {
+    Clouds: 'cloudy',
+    Clear: 'day-sunny',
+    Atmosphere: 'cloudy-gusts',
+    Snow: 'snow',
+    Rain: 'rains',
+    Drizzle: 'rain',
+    Thunderstorm: 'lightning',
+};
 
 export default function App() {
     const [city, setCity] = useState('Loading...');
     const [days, setDays] = useState([]);
     const [ok, setOk] = useState(true);
-
     const getWeather = async () => {
-        const granted = await Location.requestForegroundPermissionsAsync();
+        const { granted } = await Location.requestForegroundPermissionsAsync();
         if (!granted) {
             setOk(false);
         }
@@ -22,18 +33,17 @@ export default function App() {
         const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
         setCity(location[0].city);
         const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}`
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
         );
         const json = await response.json();
-        console.log(json);
+        console.log(json.weather);
     };
-
     useEffect(() => {
         getWeather();
     }, []);
-
     return (
         <View style={styles.container}>
+            <StatusBar style="light" />
             <View style={styles.city}>
                 <Text style={styles.cityName}>{city}</Text>
             </View>
@@ -44,13 +54,24 @@ export default function App() {
                 contentContainerStyle={styles.weather}
             >
                 {days.length === 0 ? (
-                    <View style={styles.day}>
-                        <ActivityIndicator color="black" style={{ marginTop: 10 }} size="large" />
+                    <View style={{ ...styles.day, alignItems: 'center' }}>
+                        <ActivityIndicator color="white" style={{ marginTop: 10 }} size="large" />
                     </View>
                 ) : (
                     days.map((day, index) => (
                         <View key={index} style={styles.day}>
-                            <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                            <View
+                                style={{
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    width: '100%',
+                                    justifyContent: 'space-between',
+                                }}
+                            >
+                                <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+                                <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+                            </View>
+
                             <Text style={styles.description}>{day.weather[0].main}</Text>
                             <Text style={styles.tinyText}>{day.weather[0].description}</Text>
                         </View>
@@ -64,7 +85,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'yellow',
+        backgroundColor: 'tomato',
     },
     city: {
         flex: 1.2,
@@ -74,22 +95,30 @@ const styles = StyleSheet.create({
     cityName: {
         fontSize: 58,
         fontWeight: '500',
+        color: 'white',
     },
     weather: {},
     day: {
-        width: screenWidth,
-        alignItems: 'center',
+        width: SCREEN_WIDTH,
+        alignItems: 'flex-start',
+        paddingHorizontal: 20,
     },
     temp: {
         marginTop: 50,
         fontWeight: '600',
-        fontSize: 178,
+        fontSize: 100,
+        color: 'white',
     },
     description: {
-        marginTop: -20,
-        fontSize: 60,
+        marginTop: -10,
+        fontSize: 30,
+        color: 'white',
+        fontWeight: '500',
     },
     tinyText: {
-        fontSize: 20,
+        marginTop: -5,
+        fontSize: 25,
+        color: 'white',
+        fontWeight: '500',
     },
 });
